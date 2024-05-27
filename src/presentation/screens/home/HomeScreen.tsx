cbfd42d4dd7c14320f52e1ae9f823eee4f20a1e8
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-05-27 17:41:49 -0600

    React Native Paper

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,30 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
+import IconIon from 'react-native-vector-icons/Ionicons';
+import IconMaterial from 'react-native-vector-icons/MaterialIcons';
+
+
+
 
 export const HomeScreen = () => {
     return (
         <View>
             <Text>HomeScreen</Text>
+            <Button
+                icon={() => <IconMaterial name="camera" size={20} color={"white"} />}
+                mode="contained" 
+                onPress={() => console.log('Pressed')}
+            >
+                Register me! 
+            </Button>
+            <Button 
+                style={{marginTop: 10}} 
+                icon={() => <IconIon name="camera-outline" size={20} color={"white"} />}
+                mode="contained" 
+                onPress={() => console.log('Pressed')}
+            >
+                Register me! 
+            </Button>
         </View>
     );
 };
