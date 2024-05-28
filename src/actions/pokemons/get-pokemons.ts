4c1248e1b529b578a0c7ec346d412f5522c34025
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-05-28 15:34:58 -0600

    Interfaces de PokeAPI

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,13 +1,22 @@
 import { pokeApi } from "../../config/api/pokeApi";
-import { Pokemon } from "../../domain/entities/pokemon";
+import type { Pokemon } from "../../domain/entities/pokemon";
+import type { PokeAPIPaginateResponse, PokeAPIPokemon } from "../../infrastructure/interfaces/pokeapi.interfaces";
 
 
-export const getPokemons = async ():Promise<Pokemon[]> => {
+export const getPokemons = async (page: number, limit: number = 20):Promise<Pokemon[]> => {
     try {
-        const url = '/pokemon';
-        const { data } = await pokeApi.get(url);
+        const url = `/pokemon?offset=${ page * 10 }&limit=${ limit }`;
+        const { data } = await pokeApi.get<PokeAPIPaginateResponse>(url);
 
-        console.log(data);
+        const pokemonPromises = data.results.map((info) => {
+            return pokeApi.get<PokeAPIPokemon>(info.url)
+        })
+
+        const pokeApiPokemons = await Promise.all(pokemonPromises);
+
+        console.log(pokeApiPokemons)
+
+        //console.log(data);
         
         return [];
     } catch (error) {
