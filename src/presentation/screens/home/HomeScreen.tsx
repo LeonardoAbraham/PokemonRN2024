3d5354be0797d1fda9e5293aeeeede3ebc349e2a
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-05-28 09:09:09 -0600

    Interfaces, entidades y petición http

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,8 +1,13 @@
 import { View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
+import { getPokemons } from '../../../actions/pokemons';
 
 
 export const HomeScreen = () => {
+
+
+    getPokemons();
+
     return (
         <View>
             <Text variant='displaySmall'>HomeScreen</Text>
