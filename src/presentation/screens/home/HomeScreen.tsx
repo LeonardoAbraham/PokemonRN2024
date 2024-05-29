9a5910960d698924cd514077b2d2cba0cdf05fae
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-05-29 16:51:18 -0600

    Diseño de HomeScreen parte 2

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -3,20 +3,39 @@ import { ActivityIndicator, Button, Text } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
 import { useQuery } from '@tanstack/react-query';
 import { PokeballBg } from '../../components/ui/PokeballBg';
+import { FlatList } from 'react-native-gesture-handler';
+import { globalTheme } from '../../../config/theme/global-theme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PokemonCard } from '../../components/pokemons/PokemonCard';
 
 
 export const HomeScreen = () => {
 
-    const {isLoading, data = []} = useQuery({
+    const { top } = useSafeAreaInsets();
+
+    const {isLoading, data: pokemons = []} = useQuery({
         queryKey:['pokemons'],
         queryFn: () => getPokemons(0),
         staleTime: 1000 * 60 * 60 //60 minutos
     });
 
     return (
-        <View style={{}}>
+        <View style={globalTheme.globalMargin}>
             <PokeballBg style={ styles.imgPosition } />
-            
+            <FlatList 
+                data={pokemons}
+                keyExtractor={(pokemon, index) => `${pokemon.id}- ${index}`}
+                numColumns={2}
+                style={{paddingTop: top + 20}}
+                ListHeaderComponent={
+                    () => (
+                        <Text variant='displayMedium'>Pokedex</Text>
+                    )
+                }
+                renderItem={({item}) => (
+                    <PokemonCard pokemon={item} />
+                )}
+            />
         </View>
     );
 };
