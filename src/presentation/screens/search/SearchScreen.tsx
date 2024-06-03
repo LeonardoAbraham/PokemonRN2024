4a728ca78578afb88a60e3ef136b70904c1649ad
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-06-03 13:37:22 -0600

    Precargar la informacion para buscar por nombre

--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -1,12 +1,23 @@
 import { FlatList, View } from 'react-native';
 import { globalTheme } from '../../../config/theme/global-theme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { TextInput, ActivityIndicator } from 'react-native-paper';
+import { TextInput, ActivityIndicator, Text } from 'react-native-paper';
 import { Pokemon } from '../../../domain/entities/pokemon';
 import { PokemonCard } from '../../components/pokemons/PokemonCard';
+import { useQuery } from '@tanstack/react-query';
+import { getPokemonNamesWithId } from '../../../actions/pokemons';
 
 export const SearchScreen = () => {
     const { top } = useSafeAreaInsets();
+
+    const {isLoading, data: pokemonNameList = []} = useQuery({
+        queryKey:['pokemons', 'all'],
+        queryFn: () => getPokemonNamesWithId()
+    });
+
+    console.log(pokemonNameList)
+
+
     return (
         <View style={[globalTheme.globalMargin, { paddingTop: top +10 }]}>
             <TextInput 
@@ -19,6 +30,8 @@ export const SearchScreen = () => {
             />
 
             <ActivityIndicator style={{ paddingTop: 20 }} />
+
+            <Text>{ JSON.stringify(pokemonNameList, null, 2) }</Text>
 
             <FlatList 
                 data={[] as Pokemon[]}
