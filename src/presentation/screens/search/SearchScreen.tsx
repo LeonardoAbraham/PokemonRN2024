bd325df3be58c87ba1a4de68c0d93fd31364ce8c
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-06-05 11:23:44 -0600

    useDebounceValue

--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -8,10 +8,13 @@ import { useQuery } from '@tanstack/react-query';
 import { getPokemonNamesWithId, getPokemonsByIds } from '../../../actions/pokemons';
 import { useMemo, useState } from 'react';
 import { FullScreenLoader } from '../../components/ui/FullScreenLoader';
+import { useDebouncedValue } from '../../hooks/useDebouncedValue';
 
 export const SearchScreen = () => {
     const { top } = useSafeAreaInsets();
-    const [term, setTerm] = useState('')
+    const [term, setTerm] = useState('');
+
+    const debouncedValue = useDebouncedValue(term);
 
     const {isLoading, data: pokemonNameList = []} = useQuery({
         queryKey:['pokemons', 'all'],
@@ -21,17 +24,17 @@ export const SearchScreen = () => {
     //TODO: Aplicar el debounce
     const pokemonNameIdList = useMemo(() => {
         //Es un número
-        if(!isNaN(Number(term))){
-            const pokemon = pokemonNameList.find(pokemon => pokemon.id === Number(term));
+        if(!isNaN(Number(debouncedValue))){
+            const pokemon = pokemonNameList.find(pokemon => pokemon.id === Number(debouncedValue));
             return pokemon ? [pokemon]:[]
         }
 
-        if(term.length === 0) return [];
+        if(debouncedValue.length === 0) return [];
 
-        if(term.length < 3) return [];
+        if(debouncedValue.length < 3) return [];
 
-        return pokemonNameList.filter(pokemon => pokemon.name.includes(term.toLocaleLowerCase()));
-    },[term]);
+        return pokemonNameList.filter(pokemon => pokemon.name.includes(debouncedValue.toLocaleLowerCase()));
+    },[debouncedValue]);
 
     const {isLoading: isLoadingPokemons, data: pokemons = []} = useQuery({
         queryKey: ['pokemons', 'by', pokemonNameIdList],
@@ -56,6 +59,7 @@ export const SearchScreen = () => {
 
             {isLoadingPokemons && <ActivityIndicator style={{ paddingTop: 20 }} />}
             
+            {/* <Text>{ JSON.stringify(pokemonNameIdList, null, 2) }</Text> */}
 
             <FlatList 
                 data={pokemons}
